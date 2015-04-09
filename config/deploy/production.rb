fe4ee6372b7b0f5b5319f932df22451834756b6e
yoshidaagri <yoshidaagri@gmail.com>
2015-04-09 22:10:35 +0900

    capistrano setting miss fix1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{yoshidaagri@52.68.44.67}
+role :web, %w{yoshidaagri@52.68.44.67}
+role :db,  %w{yoshidaagri@52.68.44.67}
 
 
 # Extended Server Syntax
