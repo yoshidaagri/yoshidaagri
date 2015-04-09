12ed53c7819f3a51b90bd3719fcddb413c9b0eb7
yoshidaagri <yoshidaagri@gmail.com>
2015-04-09 22:00:49 +0900

    capistrano syntax err fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:yoshidaagri/yoshidaagri.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/var/www/yoshidaagri
+set :deploy_to, '/var/www/yoshidaagri'
 
 # Default value for :scm is :git
 set :scm, :git
