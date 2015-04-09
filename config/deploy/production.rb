3521748c147fc5e63ce319b33d3ffbf62d947464
yoshidaagri <yoshidaagri@gmail.com>
2015-04-09 22:16:08 +0900

    capistrano setting miss fix3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '52.68.44.67', user: 'yoshidaagri', roles: %w{web app}, my_property: :my_value
+server '52.68.44.67', user: 'yoshidaagri', roles: %w{web app db}
 
 
 # Custom SSH Options
