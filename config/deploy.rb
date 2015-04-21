75a2fce63e989fb8e1daa6422dc00c0b07287fad
yoshidaagri <yoshidaagri@gmail.com>
2015-04-21 21:44:59 +0900

    capistrano setting miss fix12

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,26 +33,32 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-
-  desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
+    invoke 'unicorn:restart'
   end
+end
 
-  after :publishing, :restart
+#︎namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+#  desc 'Restart application'
+#  task :restart do
+#    on roles(:app), in: :sequence, wait: 5 do
+#      # Your restart mechanism here, for example:
+#      # execute :touch, release_path.join('tmp/restart.txt')
+#    end
+#  end
+
+#  after :publishing, :restart
+
+#  after :restart, :clear_cache do
+#    on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
-    end
-  end
+#    end
+#  end
 
-end
+#end
